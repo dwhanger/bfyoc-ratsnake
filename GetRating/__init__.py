3facc709adb1a0a7d38ce28a4e36d9e22eb1851b
Troy Hite <troyhite@microsoft.com>
2022-10-05 10:13:44 -0500

    working

--- GetRating/__init__.py
+++ GetRating/__init__.py
@@ -5,16 +5,15 @@ import azure.functions as func
 def main(req: func.HttpRequest, doc: func.DocumentList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     if not doc:
-        logging.warning("ToDo item not found")
+        logging.warning("ratingId not found")
         return func.HttpResponse(
             "Rating ID Not Found",
             status_code=400
         )
     else:
-        logging.info("Found ToDo item, Description=%s",
-                     doc[0]['description'])
+        logging.info("ratingId Found")
         return func.HttpResponse(
-             str(doc[0]['description']),
+             str(doc[0].to_json()),
              status_code=200
         )
    
