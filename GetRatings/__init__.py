aadee2c5111c6b210b73c662c5e7ebb74fc86d70
Troy Hite <troyhite@microsoft.com>
2022-10-05 11:39:27 -0500

    GetRatings updated

--- GetRatings/__init__.py
+++ GetRatings/__init__.py
@@ -1,8 +1,10 @@
 import logging
 from pydoc import doc
 import azure.functions as func
+import json
 
 def main(req: func.HttpRequest, docs: func.DocumentList) -> func.HttpResponse:
+    userId = req.params.get('userId')
     logging.info('Python HTTP trigger function processed a request.')
     if not docs:
         logging.warning("userId not found")
@@ -12,8 +14,11 @@ def main(req: func.HttpRequest, docs: func.DocumentList) -> func.HttpResponse:
         )
     else:
         logging.info("userId Found")
+        output=[]
+        for doc in docs:
+            output.append(doc.to_json())
+        logging.info(str(output))
         return func.HttpResponse(
-             str(dir(docs)), #.to_json()),
-             status_code=200
+            str(output),  
+            status_code=200
         )
-   
